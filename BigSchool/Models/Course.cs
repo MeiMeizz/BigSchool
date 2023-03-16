f5cf9a8fc3fef65fd7554c6c2c18445104d73fcf
Nguyen Thanh Tuan <nguyenthanhtuan300102@gmail.com>
2023-03-16 10:47:17 +0700

    update

--- BigSchool/Models/Course.cs
+++ BigSchool/Models/Course.cs
@@ -9,6 +9,7 @@ namespace BigSchool.Models
 {
     public class Course
     {
+        public string Name;
         public int Id { get; set; }
         public ApplicationUser Lecturer { get; set; }
         [Required]
